0e90f0a9dac10be664bd9b160563c96e7c605ed6
atibp1722 <atibp1722@gmail.com>
2026-09-02 16:00:41 +0545

    bookmark; javascript function to filter based on category, get category name that matches reference, then clear exisiting ones and display only the matching ones

--- bookmark/script.js
+++ bookmark/script.js
@@ -1,7 +1,7 @@
 // initialize and connection function to firebase 
 import { initializeApp } from 'https://gstatic.com/firebasejs/firebase-app.js';
 // import the necessary functions
-import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, serverTimeStamp } from 'https://gstatic.com/firebasejs/firebase-app.js';
+import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, query, where, serverTimeStamp } from 'https://gstatic.com/firebasejs/firebase-app.js';
 
 // project configuration settings
 const firebaseConfig = {
@@ -65,9 +65,9 @@ function showCard(){
         data.docs.forEach(document => {
             // add each newly card
             cards.innerHTML += generateTemplate(document.data(), document.id);
-            // call delete function here as without loading card first delete button is not seen
-            deleteEvent();
-        })
+        });
+        // call delete function here as without loading card first delete button is not seen
+        deleteEvent();
     })
     .catch(error => {
         console.log(error);
@@ -93,4 +93,41 @@ addForm.addEventListener("submit", event => {
         addForm.reset();
         showCard();
     }) 
+});
+
+function filteredCard(category){
+    // if all selected show everything
+    if (category === "all"){
+        showCard();
+    }else{
+        // get reference of category that matches with desired category
+        const qRef = query(colRef, where("category", "==", category));
+        cards.innerHTML = "";
+        // get all matching the reference
+        getDoc(qRef)
+            .then( data => {
+                // loop through and create card for every matching
+                data.docs.forEach(document => {
+                    cards.innerHTML += generateTemplate(document.data(), document.id);
+            });
+            deleteEvent();
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+}
+
+const catList = document.querySelector(".category-list");
+// span needed to make active the clicked one
+const catSpan = document.querySelector(".category-list span")
+
+catList.addEventListener("click", event=> {
+    if (event.target.tagName === "span"){
+        filteredCard(event.tag,innerText.toLowerCase());
+        // remove active status from each category
+        catSpan.forEach(span => span.classList.remove("active"));
+        // make active the category user has just clicked
+        event.target.classList.add("active");
+    }
 });
