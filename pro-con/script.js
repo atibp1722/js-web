25fe9dc168c04c4ee6db5a9aa309e3518b3839b9
atibp1722 <atibp1722@gmail.com>
2026-09-03 15:42:34 +0545

    list maker; javascript functions to redner items on the page and update scoreboard for who wins or looses

--- pro-con/script.js
+++ pro-con/script.js
@@ -29,3 +29,43 @@ function addItem(type){
     input.value="";
     updateScore();
 }
+
+// function to display new item on the page
+function renderItem(text, type){
+    const li = document.createElement("li");
+    li.textContent = text;
+    // create a new delete button
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete-btn");
+
+    // code when button cliked
+    deleteBtn.onclick = () => {
+        li.remove();
+        updateScore();
+    };
+    // add delete button inside <li> element
+    li.appendChild(deleteBtn);
+    // add new item to the matching list
+    document.getElementById(type+"List").appendChild(li);
+}
+
+// function to update score 
+function updateScore(){
+    // count items inside each list
+    const proCount = document.getElementById("prosList").children.length();
+    const conCount = document.getElementById("consList").children.length();
+    const scoreboard = document.getElementById("scoreboard");
+
+    // default verdict
+    let verdict = "You Decide";
+
+    // condition for which side win/loss or tie 
+    if (proCount > conCount) verdict = "Pro side wins!";
+    else if (conCount > proCount) verdict = "Con side wins!";
+    else if (proCount === conCount) verdict = "It's a tie!";
+
+    // dispaly score and verdict
+    scoreboard.innerHTML = `Pros: ${prosCount}| Cons: ${consCount}<br> ${verdict}`
+}
