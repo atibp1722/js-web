0a9a1e298a3b872c492cb9dd4a50139b733bdc56
atibp1722 <atibp1722@gmail.com>
2026-09-04 13:11:58 +0545

    snake game; function for setting apple position, grow snake with each apple it collides with and generate random apple on canvas

--- snake-game/script.js
+++ snake-game/script.js
@@ -2,6 +2,14 @@
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext('2d');
 
+// indiviual snake block taht is to be incremented as game progresses
+class SnakePart{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 // let snake refresh 7 times per second
 let speed = 7;
 
@@ -10,6 +18,14 @@ let tileCount = 20;
 let tileSize = canvas.width/tileCount - 2;
 let headX= 10;
 let headY = 10;
+// array for snake part
+const snakeParts = [];
+// initial snake length
+let tailLength = 2;
+
+// starting position of the apple
+let appleX = 5;
+let appleY = 5;
 
 // control snake movement
 let xVelocity = 0;
@@ -19,6 +35,8 @@ let yVelocity = 0;
 function drawGame(){
     clearScreen();
     changeSnakePosition();
+    checkAppleCollision();
+    drawApple();
     drawSnake();
     setTimeout(drawGame, 1000/speed);
 }
@@ -32,6 +50,35 @@ function drawSnake(){
     // put the snake in the middle of the canvas
     ctx.fillStyle = 'white';
     ctx.fillRect(headX*tileCount, headY*tileCount, tileSize, tileSize);
+
+    ctx.fillStyle = 'green';
+    // loop through snake and draw additional body
+    for(let i=0; i<snakeParts.length; i++){
+        let part = snakeParts[i];
+        ctx.fillRect(part.x*tileCount, part.y*tileCount, tileSize, tileSize);
+    }
+
+    // add current position
+    snakeParts.push(new SnakePart(headX, headY));
+    // remove if larger than allowed length
+    if(snakeParts.length>tailLength){
+        snakeParts.shift();
+    }
+}
+
+function drawApple(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize);
+}
+
+// check if snake is same tile as apple
+function checkAppleCollision(){
+    if(appleX===headX && appleY===headY){
+        // move apple to a random psition on the canvas
+        appleX = Math.floor(Math.random()*tileCount);
+        appleY = Math.floor(Math.random()*tileCount);
+        tailLength++;
+    }
 }
 
 function changeSnakePosition(){
@@ -42,7 +89,7 @@ function changeSnakePosition(){
     // move the snake vertically (up/down)
     // 1: move down
     // -1: move up
-    headX = headY + yVelocity;
+    headY = headY + yVelocity;
 }
 
 document.body.addEventListener("keydown", keyDown);
@@ -51,6 +98,8 @@ document.body.addEventListener("keydown", keyDown);
 function keyDown(event){
     // up arrow pressed
     if(event.keyCode == 38){
+        if(yVelocity == 1)
+            return;
         // move up
         yVelocity = -1;
         // stop horizontal movement
@@ -58,11 +107,15 @@ function keyDown(event){
     }
     // down arrow pressed
     if(event.keyCode == 40){
+        if(yVelocity == -1)
+            return;
         yVelocity = 1;
         xVelocity = 0;
     }
     // left arrow pressed
     if(event.keyCode == 37){
+        if(xVelocity == 1)
+            return;
         // stop vertical movement
         yVelocity = 0;
         // move left
@@ -70,6 +123,8 @@ function keyDown(event){
     }
     // right arrow pressed
      if(event.keyCode == 39){
+        if(xVelocity == -1)
+            return;
         yVelocity = 0;
         xVelocity = 1;
     }
