9dccbbcf7f88ff68eb7c5ea2a55047e944f33d0f
atibp1722 <atibp1722@gmail.com>
2026-09-04 12:25:41 +0545

    snake game; function for controlling snake movement and listen for key pressed and move snake accordingly

--- snake-game/script.js
+++ snake-game/script.js
@@ -11,9 +11,14 @@ let tileSize = canvas.width/tileCount - 2;
 let headX= 10;
 let headY = 10;
 
+// control snake movement
+let xVelocity = 0;
+let yVelocity = 0;
+
 // game loop
 function drawGame(){
     clearScreen();
+    changeSnakePosition();
     drawSnake();
     setTimeout(drawGame, 1000/speed);
 }
@@ -27,6 +32,48 @@ function drawSnake(){
     // put the snake in the middle of the canvas
     ctx.fillStyle = 'white';
     ctx.fillRect(headX*tileCount, headY*tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition(){
+    // move the snake horizontally (left/right)
+    // 1: move right
+    // -1: move left
+    headX = headX + xVelocity;
+    // move the snake vertically (up/down)
+    // 1: move down
+    // -1: move up
+    headX = headY + yVelocity;
+}
+
+document.body.addEventListener("keydown", keyDown);
+
+// function to listen to key pressed
+function keyDown(event){
+    // up arrow pressed
+    if(event.keyCode == 38){
+        // move up
+        yVelocity = -1;
+        // stop horizontal movement
+        xVelocity = 0;
+    }
+    // down arrow pressed
+    if(event.keyCode == 40){
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+    // left arrow pressed
+    if(event.keyCode == 37){
+        // stop vertical movement
+        yVelocity = 0;
+        // move left
+        xVelocity = -1;
+    }
+    // right arrow pressed
+     if(event.keyCode == 39){
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+
 }
 
 drawGame();
