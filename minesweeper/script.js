b088923c7427a180bedd0efc0acd135b2932bfe5
atibp1722 <atibp1722@gmail.com>
2026-09-11 12:30:36 +0545

    minesweeper; function for inserting mines on the game board, get the size of board, long with row and column positions, store them in a set and update cell neighbors and reduce game cells

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -21,3 +21,32 @@ function initGameStart({width, height, minesCount}){
     return STATE;
 }
 
+// insert mines on the game board
+function insertMines(STATE, minesCount){
+    // get all the cells on game board
+    const size = STATE.fieldsLeft;
+    // set for stroing mine positions
+    // used as it cannot hold duplicate values
+    const indices = new Set();
+    // continue until unique positions are gotten
+    // add the unique position to set as the index
+    while (indices.size < minesCount){
+        const index = Math.floor(Math.random() * size);
+        indices.add(index);
+    }
+    // game board width
+    const width = STATE.fields[0].length;
+    // convert array into a game row (x)
+    // calculate column value (y)
+    for(const index of indices){
+        const y = Math.floor(index / width);
+        const x = index % width;
+        // put the mine in row and column position
+        STATE.fields[y][x] = -1;
+        // update the neighboring cells
+        updateMineNeighbors(STATE, x, y);
+    }
+    // reduce number of game cells left by mines
+    STATE.fieldsLeft -= minesCount;
+}
+
