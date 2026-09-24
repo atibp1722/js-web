011546875acce87538d04a03ae42a2b3fcf85b1d
atibp1722 <atibp1722@gmail.com>
2026-09-24 12:38:51 +0545

    photo editor; get html reference and declare variables for transformation, trigger action when image uploaded and pass it to handler, function to render image on canvas swap height and width to prevent clipping, save canvas state apply css like filters and restore canvas to previous state

---
+++ photo-editor/script.js
@@ -0,0 +1,71 @@
+// reference to html elements
+const canvas = document.getElementById("canvas");
+// canvas for image
+const context = canvas.getContext("2d");
+// reference to image actions
+const upload = document.getElementById("upload");
+const brightness = document.getElementById("brightness");
+const contrast = document.getElementById("contrast");
+const saturation = document.getElementById("saturation");
+const blur = document.getElementById("blur");
+const greyscale = document.getElementById("greyscale");
+const sepia = document.getElementById("sepia");
+// object for new image
+let img = new Image();
+// image transformation variables
+let rotation = 0;
+let flipX = 1;
+let flipY = 1;
+
+// trigger when file uploaded
+upload.addEventListener("change", function(){
+    // get first file from list
+    const file = this.files[0];
+    if (!file) return;
+    // read image as data url
+    const reader = new FileReader();
+    // ready image to the handler
+    reader.onload = function(event){
+        img.onload = function(){
+            // reset slider values
+            resetEditor();
+        };
+        // assign data url to mimage
+        img.src = event.target.result;
+    };
+    // read as data url
+    reader.readAsDataURL(file);
+});
+
+// function for render image on canvas
+function draw(){
+    if (!img.src) return;
+    // rotation degree to radian for canvas
+    const angle = rotation * Math.PI / 180;
+    // image rorated by 90 or 270 degree
+    const rotated = rotation % 180 !== 0;
+    // swap height and width to prevent clipping
+    canvas.width = rotated ? img.height : img.width;
+    canvas.height = rotated ? img.width : img.height;
+    // clear canvas 
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // save current canvas before transformation
+    context.save();
+    // move to center for easy rotation
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.rotate(angle);
+    // horizontal and vertical transformation
+    context.scale(flipX, flipY);
+    // apply filter
+    context.filter = `
+                    brightness(${brightness.value}%)
+                    contrast(${contrast.value}%)
+                    saturate(${saturation.value}%)
+                    blur(${blur.value}px)
+                    grayscale(${greyscale.value}%)
+                    sepia(${sepia.value}%)`;
+    // draw image around (0, 0) coordinates
+    context.drawImage(img, -img.width / 2, -img.height / 2);
+    // canvas restore to state before
+    context.restore();
+}
